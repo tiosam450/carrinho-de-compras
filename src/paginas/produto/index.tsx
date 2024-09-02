36af852868b57e1b58efb6631d6e73790b594bfc
Samuel Nogueira <tiosam450@hotmail.com>
2024-09-01 22:47:56 -0400

    Desk Casa

--- src/paginas/produto/index.tsx
+++ src/paginas/produto/index.tsx
@@ -1,12 +1,14 @@
-import { useEffect, useState } from "react"
+import { useContext, useEffect, useState } from "react"
 import { useParams } from "react-router-dom"
-import { ProdutosProps } from "../../api/contextApi"
+import { ContextAPI, ProdutosProps } from "../../api/contextApi"
 import { api } from "../../api/services"
+import { BsCartPlus } from "react-icons/bs";
 
 
 export default function Produto(){
-    const {id}= useParams()
-    const [produto, setProduto] = useState<ProdutosProps[]>([])
+    const {id} = useParams()
+    const [produto, setProduto] = useState<ProdutosProps>()
+    const {addProduto, addCarrinho} = useContext(ContextAPI)
 
     useEffect(()=>{
         async function carregaAPI(){
@@ -14,15 +16,20 @@ export default function Produto(){
             setProduto(response.data)
         }
 
-        carregaAPI()
-
-        
+        carregaAPI()        
     },[])
-    
-    console.log(produto)
+
+
     return(
         <>
-        <h1>Produto {id}</h1>
+        <img src={produto?.cover} alt="" />
+        <h1>{produto?.title}</h1>
+        <p>{produto?.description}</p>
+        <div className="flex items-center gap-2">
+                        <strong>{produto?.price.toLocaleString('pt-BR', {style:'currency', currency:'BRL'} )}</strong>
+                        <button onClick={()=>addCarrinho} className="bg-black text-white p-[5px] rounded-md hover:bg-gray-700 transition-all ease-in-out duration-200"><BsCartPlus /></button>
+                    </div>
+        <strong>{produto?.price.toLocaleString('pt-BR',{style: 'currency', currency:'BRL'})}</strong>
         </>
     )
 }
